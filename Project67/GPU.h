64e6749efcc6b8893706132d22582d6fb7aecc7d
Vika <vikapolischuk2005@gamil.com>
2024-09-26 13:44:14 +0300

    Добавьте файлы проекта.

---
+++ Project67/GPU.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef GPU_H
+#define GPU_H
+
+class GPU
+{
+private:
+    char* name;
+    int memorySize; // В мегабайтах
+
+public:
+    GPU(const char* name, int memorySize);
+    ~GPU();
+
+    const char* getName() const;
+    int getMemorySize() const;
+
+    void setName(const char* name);
+    void setMemorySize(int memorySize);
+};
+
+#endif 
